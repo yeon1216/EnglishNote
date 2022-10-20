8b8742b5990023045860fe2187e0c94111443cd5
yeon1216 <sykim8770@gmail.com>
2022-10-20 15:03:50 +0900

    Initial Commit

---
+++ EnglishNote/EnglishNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishNoteApp.swift
+//  EnglishNote
+//
+//  Created by BOBBY.KIM on 2022/10/20.
+//
+
+import SwiftUI
+
+@main
+struct EnglishNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
